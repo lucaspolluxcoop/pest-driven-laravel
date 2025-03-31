1048d705d6f7a04723462d0f9e2eecd0cd16d58c
Lucas Manuel <lucas@polluxcoop.com>
2025-03-30 21:28:22 -0300

    feat: test if users can see and update their portfolios and others portfolios

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,7 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Auth;
+
 abstract class Controller
 {
-    //
+    public $isAdminUser = false;
+
+    public function __construct()
+    {
+        app()->call(function () {
+            $this->isAdminUser = Auth::check() && Auth::user()->isAdmin();
+        });
+    }
 }
